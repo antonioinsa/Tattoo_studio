3394f69b03d11eaf225dcfb2a86e0996e40f50f0
Toni <antonioinsa@tutanota.com>
2023-11-01 19:48:15 +0100

    feat:added auth to delete workers only for admin and superAdmin

--- src/controllers/workersController.ts
+++ src/controllers/workersController.ts
@@ -35,7 +35,7 @@ const register = async (req: Request, res: Response) => {
         const phone = req.body.phone
         const nationality = req.body.nationality
         const password = req.body.password
-        
+
         const emailRegex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,20}$/;
 
         if (!emailRegex.test(email)) {
@@ -107,7 +107,7 @@ const login = async (req: Request, res: Response) => {
                     email: worker.email,
                     role: worker.role
                 },
-                "D_uR9_M0",
+                "D_uR9_M0#@951hKEc_9l",
                 {
                     expiresIn: "8h"
                 }
@@ -133,102 +133,122 @@ const login = async (req: Request, res: Response) => {
     }
 }
 
-//
-//const updateWorkerById = async (req: Request, res: Response) => {
-//    try {
-//        const workerId = req.params.id
-//        const { first_name, last_name, phone, email, password, nationality } = req.body
-//    
-//        await Worker.findOneBy
-//            (
-//                { id: req.body.id }
-//
-//            )
-//
-//        if (password) {
-//            const encryptedPassword = bcrypt.hashSync(password, 10)
-//            await Worker.update
-//                (
-//                    { id: req.body.id },
-//                    {
-//                        password: encryptedPassword
-//                    }
-//                )
-//            const updateWorker = await Worker.findOneBy
-//                (
-//                    { id: req.token.id }
-//                )
-//            return res.status(200).json({
-//                success: true,
-//                message: `Password worker ${workerId} has been successfully updated`,
-//                data: updateWorker
-//            });
-//        }
-//        if (email) {
-//            const emailRegex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,20}$/;
-//
-//            if (!emailRegex.test(email)) {
-//
-//
-//                return res.status(400).json
-//                    (
-//                        {
-//                            sucsess: false,
-//                            message: 'Invalid email'
-//                        }
-//                    )
-//            }
-//        }
-//
-//        await Worker.update
-//            (
-//                { id: req.body.id },
-//                {
-//                    first_name: first_name,
-//                    last_name: last_name,
-//                    email: email,
-//                    phone: phone,
-//                    nationality: nationality
-//                }
-//            )
-//        const updateWorker = await Worker.findOneBy
-//            (
-//                { id: req.body.id }
-//            )
-//        return res.status(200).json({
-//            success: true,
-//            message: `${workerId} updated successfully`,
-//            data: updateWorker
-//        });
-//
-//    } catch (error) {
-//        return res.status(500).json({
-//            success: false,
-//            message: "Worker profile cant be updated",
-//            error: error
-//        });
-//    }
-//}
-//
-//const deleteWorkerById = async (req: Request, res: Response) => {
-//    try {
-//        //RECUPERO EL ID QUE QUIERO BORRAR
-//        const workerToDelete = req.params.id;
-//        //RECUPERO EL ELEMENTO QUE QUIERO ELIMINAR (COMPRUEBO)
-//        const workerToRemove = await Worker.findOneBy({
-//            //UNA VEZ RECUPERADO, SE ELIMINA (TAMBIEN SE PASA DE STRING A INT)
-//            id: parseInt(workerToDelete),
-//        })
-//
-//        const workerRemoved = await Worker.remove(workerToRemove as Worker);
-//        
-//        return res.send(workerRemoved)
-//
-//    } catch (error) {
-//
-//        return res.send(error);
-//
-//    }
-//};
-//
-export { getWorkers, register, login} //, updateWorkerById, deleteWorkerById }
+
+const updateWorkerById = async (req: Request, res: Response) => {
+    try {
+        const workerId = req.params.id
+        const { first_name, last_name, phone, email, password, nationality } = req.body
+
+        await Worker.findOneBy
+            (
+                { id: req.body.id }
+
+            )
+
+        if (password) {
+            const encryptedPassword = bcrypt.hashSync(password, 10)
+            await Worker.update
+                (
+                    { id: req.body.id },
+                    {
+                        password: encryptedPassword
+                    }
+                )
+            const updateWorker = await Worker.findOneBy
+                (
+                    { id: req.token.id }
+                )
+            return res.status(200).json({
+                success: true,
+                message: `Password worker ${workerId} has been successfully updated`,
+                data: updateWorker
+            });
+        }
+        if (email) {
+            const emailRegex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,20}$/;
+
+            if (!emailRegex.test(email)) {
+
+
+                return res.status(400).json
+                    (
+                        {
+                            sucsess: false,
+                            message: 'Invalid email'
+                        }
+                    )
+            }
+        }
+
+        await Worker.update
+            (
+                { id: req.body.id },
+                {
+                    first_name: first_name,
+                    last_name: last_name,
+                    email: email,
+                    phone: phone,
+                    nationality: nationality
+                }
+            )
+        const updateWorker = await Worker.findOneBy
+            (
+                { id: req.body.id }
+            )
+        return res.status(200).json({
+            success: true,
+            message: `${workerId} updated successfully`,
+            data: updateWorker
+        });
+
+    } catch (error) {
+        return res.status(500).json({
+            success: false,
+            message: "Worker profile cant be updated",
+            error: error
+        });
+    }
+}
+
+const deleteWorkerById = async (req: Request, res: Response) => {
+    try {
+        //RECUPERO EL ID QUE QUIERO BORRAR
+        const workerToDelete = req.body.id;
+        //RECUPERO EL ELEMENTO QUE QUIERO ELIMINAR (COMPRUEBO)
+        const workerToRemove = await Worker.findOneBy
+            (
+                { id: parseInt(workerToDelete) }
+            )
+        if (!workerToRemove) {
+            return res.status(404).json
+                (
+                    {
+                        success: false,
+                        message: (`Worker ID ${workerToDelete} not found`)
+                    }
+                )
+        }
+        const workerRemoved = await Worker.remove(workerToRemove as Worker);
+
+        return res.status(200).json
+            (
+                {
+                    success: true,
+                    message: (`Worker ID ${workerToDelete} has been deleted`),
+                    data: workerRemoved
+                }
+            )
+
+    } catch (error) {
+        return res.status(500).json
+            (
+                {
+                    success: false,
+                    message: "Worker couldn't be deleted",
+                    error: error
+                }
+            )
+    }
+}
+
+export { getWorkers, register, login, updateWorkerById, deleteWorkerById }
