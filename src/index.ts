57db7d77842b5287c17eb8031eee21b4c4722e8f
Toni <antonioinsa@tutanota.com>
2023-11-10 10:01:50 +0100

    feat:add core package for fix frontend connection

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,15 @@
 import express from "express"
+import cors from "cors"
 import { router as routerClients } from "./routes/clientsRoutes";
 import { router as routerWorkers } from "./routes/workersRoutes";
 import { router as routerAppointment } from "./routes/appointmentRoutes";
 import { AppDataSource } from "./db";
 
 const app = express()
-
 const PORT = process.env.PORT || 3000
 
 app.use(express.json())
+app.use(cors())
 app.use('/client', routerClients)
 app.use('/appointment', routerAppointment)
 app.use('/worker', routerWorkers)
